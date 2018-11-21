6a7a4984432b9ce71e8548377eed4bab81275e3e
Yeonggyun.Kim <kygha7205@naver.com>
2018-11-21 22:29:56 +0900

    DB_Connect TEST

--- server.js
+++ server.js
@@ -18,28 +18,37 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
 
-//-hus-cdbr-iron-east-01.cleardb.net -ubf543e9cd3c6f8 -p
+// -hus-cdbr-iron-east-01.cleardb.net -ubf543e9cd3c6f8 -p
 // CONNECT DB
-// var conn = mysql.createConnection({
-// 	host: "us-cdbr-iron-east-01.cleardb.net",
-// 	user: "bf543e9cd3c6f8",
-// 	password: "0dabca01"
-// });
-// conn.connect(function (err) {
-// 	if (err) throw err;
-// 	console.log('DB Connected!');
-// });
+var conn = mysql.createConnection({
+	host: "us-cdbr-iron-east-01.cleardb.net",
+	user: "bf543e9cd3c6f8",
+	password: "0dabca01",
+	database: "heroku_d9e757d3af4c794"
+});
+conn.connect(function (err) {
+	if (err) throw err;
+	console.log('DB Connected!');
+});
 
 
 // ROUTING
 //라우팅 분리
 var indexRouter = require('./routes/index.js');
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/index', indexRouter);
 
-// app.get('/', function (req, res) {
-// 	res.render('index.ejs');
-// });
+app.get('/', function (req, res) {
+	res.render('index.ejs');
+	var sql = "INSERT INTO breakfast (how_many, count) values (1, 1);";
+	conn.query(sql, function (err, rows, fields){
+		if(err){
+			console.log(err);
+		} else {
+			console.log("INSERT SUCCESS!!!");
+		}
+	} );
+});
 
 // app.get('/index', function(req, res){
 // 	res.render('index.ejs');
