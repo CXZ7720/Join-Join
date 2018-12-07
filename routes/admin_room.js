f54e32dd7255b7548c9639347a89c78856d275bf
Yeonggyun.Kim <kygha7205@naver.com>
2018-12-08 04:19:47 +0900

    Finish admin room management page

--- routes/admin_room.js
+++ routes/admin_room.js
@@ -1,7 +1,34 @@
 var router = require('express').Router();
+var conn = require('./db');
+const queryPromise = (query) => {
+    return new Promise((resolve, reject) => {
+        conn.query(query, (err, res) => {
+            if (err) reject(err)
+            else resolve(res)
+        })
+    })
+}
+
 
 router.get('/', function (req, res) {
-    res.render('ad-room');
+    
+    var date = new Date().toISOString().split('T')[0];
+    console.log(date);
+    var reservation_query = `SELECT room_number, food_count, DATE_FORMAT(check_in, "%Y-%m-%d") AS check_in, DATE_FORMAT(check_out, "%Y-%m-%d") AS check_out, how_many, extra_service FROM Reservation WHERE check_in <= '${date}' AND check_out >= '${date}' order by room_number;`;
+    // console.log(reservation_query);
+    queryPromise(reservation_query)
+        .then((queryResult) => {
+            // console.log(queryResult);
+            res.render('ad-room',{
+                reservation : queryResult
+            });
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+
+
+
 });
 
 module.exports = router;
